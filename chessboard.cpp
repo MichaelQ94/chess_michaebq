d26ef6cdcd92de933c72a1e71c9e4d067ba90646
Michael Qian <michaebq@usc.edu>
2013-04-26 02:27:50 -0700

    changed organization of chess pieces, now stored in two arrays of chess piece pointers (white and black)

--- chessboard.cpp
+++ chessboard.cpp
@@ -2,84 +2,136 @@
 
 ChessBoard::ChessBoard()
 {
-	//instantiate white pieces
-	WP1 = new ChessPiece_Pawn('W');
-	WP2 = new ChessPiece_Pawn('W');
-	WP3 = new ChessPiece_Pawn('W');
-	WP4 = new ChessPiece_Pawn('W');
-	WP5 = new ChessPiece_Pawn('W');
-	WP6 = new ChessPiece_Pawn('W');
-	WP7 = new ChessPiece_Pawn('W');
-	WP8 = new ChessPiece_Pawn('W');
+	whiteToMove_ = true;
 	
-	WN1 = new ChessPiece_Knight('W');
-	WN2 = new ChessPiece_Knight('W');
+	//instantiate and allocate memory for white pieces
+	whitePieces_[0] = new ChessPiece_Pawn('W');
+	whitePieces_[1]  = new ChessPiece_Pawn('W');
+	whitePieces_[2]  = new ChessPiece_Pawn('W');
+	whitePieces_[3]  = new ChessPiece_Pawn('W');
+	whitePieces_[4]  = new ChessPiece_Pawn('W');
+	whitePieces_[5]  = new ChessPiece_Pawn('W');
+	whitePieces_[6]  = new ChessPiece_Pawn('W');
+	whitePieces_[7]  = new ChessPiece_Pawn('W');
 	
-	WB1 = new ChessPiece_Bishop('W');
-	WB2 = new ChessPiece_Bishop('W');
+	whitePieces_[8]  = new ChessPiece_Knight('W');
+	whitePieces_[9]  = new ChessPiece_Knight('W');
 	
-	WQ = new ChessPiece_Queen('W');
+	whitePieces_[10]  = new ChessPiece_Bishop('W');
+	whitePieces_[11]  = new ChessPiece_Bishop('W');
 	
-	WK = new ChessPiece_King('W');
+	whitePieces_[12]  = new ChessPiece_Rook('W');
+	whitePieces_[13]  = new ChessPiece_Rook('W');
 	
-	//instantiate black pieces
-	BP1 = new ChessPiece_Pawn('B');
-	BP2 = new ChessPiece_Pawn('B');
-	BP3 = new ChessPiece_Pawn('B');
-	BP4 = new ChessPiece_Pawn('B');
-	BP5 = new ChessPiece_Pawn('B');
-	BP6 = new ChessPiece_Pawn('B');
-	BP7 = new ChessPiece_Pawn('B');
-	BP8 = new ChessPiece_Pawn('B');
+	whitePieces_[14]  = new ChessPiece_Queen('W');
 	
-	BN1 = new ChessPiece_Knight('B');
-	BN2 = new ChessPiece_Knight('B');
+	whitePieces_[15]  = new ChessPiece_King('W');
 	
-	BB1 = new ChessPiece_Bishop('B');
-	BB2 = new ChessPiece_Bishop('B');
+	//instantiate and allocate memory for black pieces
+	blackPieces_[0] = new ChessPiece_Pawn('B');
+	blackPieces_[1] = new ChessPiece_Pawn('B');
+	blackPieces_[2] = new ChessPiece_Pawn('B');
+	blackPieces_[3] = new ChessPiece_Pawn('B');
+	blackPieces_[4] = new ChessPiece_Pawn('B');
+	blackPieces_[5] = new ChessPiece_Pawn('B');
+	blackPieces_[6] = new ChessPiece_Pawn('B');
+	blackPieces_[7] = new ChessPiece_Pawn('B');
 	
-	BQ = new ChessPiece_Queen('B');
+	blackPieces_[8] = new ChessPiece_Knight('B');
+	blackPieces_[9] = new ChessPiece_Knight('B');
 	
-	BK = new ChessPiece_King('B');
+	blackPieces_[10] = new ChessPiece_Bishop('B');
+	blackPieces_[11] = new ChessPiece_Bishop('B');
 	
+	blackPieces_[12] = new ChessPiece_Rook('B');
+	blackPieces_[13] = new ChessPiece_Rook('B');
 	
-	//create the board squares and place the pieces on them
+	blackPieces_[14] = new ChessPiece_Queen('B');
+	
+	blackPieces_[15] = new ChessPiece_King('B');
+	
+	//instantiate allocate memory for board squares
 	for(int i = 0; i < 64; ++i)
 	{
-		squares_[i] = new ChessBoardSquare(this);
+		squares_[i] = new ChessBoardSquare(this, i);
 	}
 	
-	squares_[0]->setPiece(WR1);
-	squares_[1]->setPiece(WN1);
-	squares_[2]->setPiece(WN1);
-	squares_[3]->setPiece(WQ);
-	squares_[4]->setPiece(WK);
-	squares_[5]->setPiece(WB2);
-	squares_[6]->setPiece(WN2);
-	squares_[7]->setPiece(WR2);
-	squares_[8]->setPiece(WP1);
-	squares_[9]->setPiece(WP2);
-	squares_[10]->setPiece(WP3);
-	squares_[11]->setPiece(WP4);
-	squares_[12]->setPiece(WP5);
-	squares_[13]->setPiece(WP6);
-	squares_[14]->setPiece(WP7);
-	squares_[15]->setPiece(WP8);
+	//place the pieces on the board
+	whitePieces_[12]->move(squares_[0]); //white left rook
+	whitePieces_[8]->move(squares_[1]); //white left knight
+	whitePieces_[10]->move(squares_[2]); //white left bishop
+	whitePieces_[14]->move(squares_[3]); //white queen
+	whitePieces_[15]->move(squares_[4]); //white king
+	whitePieces_[11]->move(squares_[5]); //white right bishop
+	whitePieces_[9]->move(squares_[6]); //white right knight
+	whitePieces_[13]->move(squares_[7]); //white right rook
+	//white pawns
+	whitePieces_[0]->move(squares_[8]);
+	whitePieces_[1]->move(squares_[9]);
+	whitePieces_[2]->move(squares_[10]);
+	whitePieces_[3]->move(squares_[11]);
+	whitePieces_[4]->move(squares_[12]);
+	whitePieces_[5]->move(squares_[13]);
+	whitePieces_[6]->move(squares_[14]);
+	whitePieces_[7]->move(squares_[15]);
 	
-	squares_[48]->setPiece(BP1);
-	squares_[49]->setPiece(BP2);
-	squares_[50]->setPiece(BP3);
-	squares_[51]->setPiece(BP4);
-	squares_[52]->setPiece(BP5);
-	squares_[53]->setPiece(BP6);
-	squares_[54]->setPiece(BP7);
-	squares_[55]->setPiece(BP8);
-	squares_[56]->setPiece(BR1);
-	squares_[57]->setPiece(BN1);
-	squares_[58]->setPiece(BB1);
-	squares_[59]->setPiece(BQ);
-	squares_[60]->setPiece(BK);
-	squares_[63]->setPiece(BB2);
-	squares_[62]->setPiece(BN2);
-	squares_[63]->setPiece(BR2);
+	//black pawns
+	blackPieces_[0]->move(squares_[48]);
+	blackPieces_[1]->move(squares_[49]);
+	blackPieces_[2]->move(squares_[50]);
+	blackPieces_[3]->move(squares_[51]);
+	blackPieces_[4]->move(squares_[52]);
+	blackPieces_[5]->move(squares_[53]);
+	blackPieces_[6]->move(squares_[54]);
+	blackPieces_[7]->move(squares_[55]);
+	
+	blackPieces_[12]->move(squares_[56]); //black left rook
+	blackPieces_[8]->move(squares_[57]); //black left knight
+	blackPieces_[10]->move(squares_[58]); //black left bishop
+	blackPieces_[14]->move(squares_[59]); //black queen
+	blackPieces_[15]->move(squares_[60]); //black king
+	blackPieces_[11]->move(squares_[61]); //black right bishop
+	blackPieces_[9]->move(squares_[62]); //black right knight
+	blackPieces_[13]->move(squares_[63]); //black right rook
+}
+
+bool ChessBoard::whiteToMove()
+{
+	return whiteToMove_;
+}
+
+void ChessBoard::changeTurn()
+{
+	whiteToMove_ = !whiteToMove_;
+}
+
+ChessBoardSquare* ChessBoard::square(int index)
+{
+	return squares_[index];
+}
+
+ChessPiece* ChessBoard::whitePiece(int index)
+{
+	return whitePieces_[index];
+}
+
+ChessPiece* ChessBoard::blackPiece(int index)
+{
+	return blackPieces_[index];
+}
+
+ChessBoard::~ChessBoard()
+{
+	//delete dynamically allocated squares
+	for(int i = 0; i < 64; ++i)
+	{
+		delete squares_[i];
+	}
+	
+	//delete dynamically allocated pieces
+	for(int i = 0; i < 16; ++i)
+	{
+		delete whitePieces_[i];
+		delete blackPieces_[i];
+	}
 }
--- chessboard.h
+++ chessboard.h
@@ -1,6 +1,7 @@
 #ifndef CHESSBOARD_H
 #define CHESSBOARD_H
 
+#include <iostream>
 #include "chesspiece.h"
 
 class ChessBoardSquare;
@@ -15,24 +16,24 @@ class ChessPiece_King;
 class ChessBoard
 {
 	private:
+		bool whiteToMove_; //true on white's turn, false on black's turn
 		ChessBoardSquare* squares_[64];
-		//white pieces
-		ChessPiece_Pawn *WP1, *WP2, *WP3, *WP4, *WP5, *WP6, *WP7, *WP8;
-		ChessPiece_Knight *WN1, *WN2;
-		ChessPiece_Bishop *WB1, *WB2;
-		ChessPiece_Rook *WR1, *WR2;
-		ChessPiece_Queen *WQ;
-		ChessPiece_King *WK;
-		
-		//black pieces
-		ChessPiece_Pawn *BP1, *BP2, *BP3, *BP4, *BP5, *BP6, *BP7, *BP8;
-		ChessPiece_Knight *BN1, *BN2;
-		ChessPiece_Bishop *BB1, *BB2;
-		ChessPiece_Rook *BR1, *BR2;
-		ChessPiece_Queen *BQ;
-		ChessPiece_King *BK;
+		ChessPiece* whitePieces_[16];
+		ChessPiece* blackPieces_[16];
+		//Key:
+		//0-7: pawns
+		//8,9: knights
+		//10,11: bishops
+		//12,13: rooks
+		//14: queen
+		//15: king
 	public:
 		ChessBoard();
+		ChessBoardSquare* square(int index);
+		ChessPiece* whitePiece(int index);
+		ChessPiece* blackPiece(int index);
+		bool whiteToMove();
+		void changeTurn();
 		~ChessBoard();
 };
 #endif
