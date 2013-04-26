31833224dc761c36375ff41d5c23389d1ac57455
Michael Qian <michaebq@usc.edu>
2013-04-26 02:33:06 -0700

    added some additional features. This will be the class which handles user input (clicks). Current mouse click event function is a test

--- guisquare.cpp
+++ guisquare.cpp
@@ -1,1 +1,25 @@
 #include "guisquare.h"
+
+GUISquare::GUISquare(GraphicsWindow *gw, ChessBoardSquare *square, int x, int y, int w) : QGraphicsRectItem(x, y, w, w)
+{
+	gw_ = gw;
+	square_ = square;
+	x_ = x;
+	y_ = y;
+}
+
+void GUISquare::mousePressEvent(QGraphicsSceneMouseEvent *c)
+{
+	c->pos();
+	this->setBrush(QBrush(Qt::cyan));
+}
+
+int GUISquare::getX()
+{
+	return x_;
+}
+
+int GUISquare::getY()
+{
+	return y_;
+}
--- guisquare.h
+++ guisquare.h
@@ -11,9 +11,11 @@
 #include <QGraphicsItemAnimation>
 #include <QPushButton>
 #include <QPixmap>
+#include <QString>
 #include <vector>
 #include <QGraphicsSceneMouseEvent>
 #include "graphicswindow.h"
+#include "guipiece.h"
 #include "chessboardsquare.h"
 
 class GraphicsWindow;
@@ -23,9 +25,14 @@ class GUISquare : public QGraphicsRectItem
 	private:
 		GraphicsWindow *gw_;
 		ChessBoardSquare *square_;
+		int x_, y_;
 	
 	public:
+		GUISquare(GraphicsWindow*, ChessBoardSquare*, int x, int y, int w);
+		int getX();
+		int getY();
 		
-	
+	public slots:
+		void mousePressEvent(QGraphicsSceneMouseEvent *c);
 };
 #endif
